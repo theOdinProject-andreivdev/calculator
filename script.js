6b50cdeb80b6838c7dea807c017b8702b7902a24
Andrei Voinea <andreivdev@gmail.com>
2020-10-31 22:07:59 +0200

    project done, but does not look pretty :(

--- script.js
+++ script.js
@@ -1,9 +1,45 @@
+//populate all our controllers
+
 const buttons = document.querySelectorAll(".button");
+const display = document.querySelector(".display");
+let numberButtons = [];
+let operatorButtons = [];
+let eqButton;
+let clearButton;
+
+buttons.forEach((button) => {
+  if (
+    button.dataset.button == "0" ||
+    button.dataset.button == "1" ||
+    button.dataset.button == "2" ||
+    button.dataset.button == "3" ||
+    button.dataset.button == "4" ||
+    button.dataset.button == "5" ||
+    button.dataset.button == "6" ||
+    button.dataset.button == "7" ||
+    button.dataset.button == "8" ||
+    button.dataset.button == "9"
+  ) {
+    numberButtons.push(button);
+  }
+  if (
+    button.dataset.button == "plus" ||
+    button.dataset.button == "minus" ||
+    button.dataset.button == "multiply" ||
+    button.dataset.button == "divide"
+  ) {
+    operatorButtons.push(button);
+  }
+  if (button.dataset.button == "eq") eqButton = button;
+  if (button.dataset.button == "clear") clearButton = button;
+});
+//--
+
+//--pretty animations for all buttons
 
 buttons.forEach((button) => {
   button.addEventListener("click", buttonEffect);
   button.addEventListener("transitionend", buttonRemoveEffect);
-  console.log("t");
 });
 
 function buttonEffect(e) {
@@ -13,3 +49,78 @@ function buttonEffect(e) {
 function buttonRemoveEffect(e) {
   this.classList.remove("buttonEffect");
 }
+
+//--
+
+//--the real magic happens here
+let currentValue = 0;
+let previousValue = 0;
+let currentOperation = "";
+
+numberButtons.forEach((button) => {
+  button.addEventListener("click", function () {
+    currentValue = currentValue * 10 + parseInt(this.dataset.button);
+    displayValue();
+  });
+});
+
+operatorButtons.forEach((button) => {
+  button.addEventListener("click", function () {
+    currentOperation = this.dataset.button;
+    previousValue = currentValue;
+    currentValue = 0;
+    displayValue();
+  });
+});
+
+eqButton.addEventListener("click", function () {
+  currentValue = operate(currentOperation, previousValue, currentValue);
+  displayValue();
+});
+
+clearButton.addEventListener("click", function () {
+  currentValue = 0;
+  previousValue = 0;
+  displayValue();
+});
+
+function displayValue() {
+  display.textContent = currentValue;
+}
+//
+
+//--basic computation functions
+function operate(operator, a, b) {
+  switch (operator) {
+    case "plus":
+      return plus(a, b);
+      break;
+    case "minus":
+      return minus(a, b);
+      break;
+    case "multiply":
+      return multiply(a, b);
+      break;
+    case "divide":
+      return divide(a, b);
+      break;
+  }
+}
+
+function plus(a, b) {
+  return a + b;
+}
+
+function minus(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+//--
